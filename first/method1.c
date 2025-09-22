34e63b40d4035b689e737b3bf8ef89ed67bf35c4
sunfl0wer <therealsunfl0wer@proton.me>
2025-09-22 16:02:14 +0300

    refactor: update method1.c and method2.c for consistent English output messages and improved readability

--- first/method1.c
+++ first/method1.c
@@ -3,25 +3,28 @@
 int main(int argc, char const *argv[])
 {
     int x, y;
-    char *invalidValue = "Задане значення x не підпадає під ОДЗ задану у частині рівняння\n\0";
+    char *invalidValue = "The entered value of x does not fall within the domain specified for this part of the equation\n\0";
 
-    printf("Введіть x: ");
+    printf("Input x: ");
     scanf("%d", &x);
 
-    if (-10 > x >= -49) {
-        if (10 >= x > 0)
-        {
+    if (x >= -49) {
+        if (x < -10) {
             y = 10 * (x*x*x) + (7 * x / 5) + 2;
-            printf("1. y(%d) = %d для y = 10x^3 + 7x/5 + 2, x ∈ [-49,-10) U (0,10]\n", x, y);
-        }
-        else
-            {
+            printf("1. y(%d) = %d for y = 10x^3 + 7x/5 + 2, x ∈ [-49,-10) U (0,10]\n", x, y);
+        } else if (x > 0) {
+            if (x <= 10) {
+                y = 10 * (x*x*x) + (7 * x / 5) + 2;
+                printf("1. y(%d) = %d for y = 10x^3 + 7x/5 + 2, x ∈ [-49,-10) U (0,10]\n", x, y);
+            } else {
                 printf("1.");
                 printf(invalidValue);
             }
-    }
-    else
-    {
+        } else {
+            printf("1.");
+            printf(invalidValue);
+        }
+    } else {
         printf("1.");
         printf(invalidValue);
     }
@@ -29,7 +32,7 @@ int main(int argc, char const *argv[])
     if (x > 20)
     {
         y = -x + 9;
-        printf("2. y(%d) = %d для y = -x + 9, x ∈ (20, + ∞)\n", x, y);
+        printf("2. y(%d) = %d for y = -x + 9, x ∈ (20, + ∞)\n", x, y);
     }
     else
     {
--- first/method2.c
+++ first/method2.c
@@ -2,13 +2,13 @@
 
 int main(int argc, char const *argv[])
 {
-    char *invalidValue = "Задане значення x не підпадає під ОДЗ задану у частині рівняння\n\0";
+    char *invalidValue = "The entered value of x does not fall within the domain specified for this part of the equation\n\0";
     int x, y;
-    printf("Введіть x: ");
+    printf("Input x: ");
     scanf("%d", &x);
     if ((-49 <= x && x < -10) || (0 < x && x <= 10)) {
         y = 10 * (x*x*x) + (7 * x / 5) + 2;
-        printf("1. y(%d) = %d для y = 10x^3 + 7x/5 + 2, x ∈ [-49,-10) U (0,10]\n", x, y);
+        printf("1. y(%d) = %d for y = 10x^3 + 7x/5 + 2, x ∈ [-49,-10) U (0,10]\n", x, y);
     }
     else
     {
@@ -19,7 +19,7 @@ int main(int argc, char const *argv[])
     if (x > 20)
     {
         y = -x + 9;
-        printf("2. y(%d) = %d для y = -x + 9, x ∈ (20, + ∞)\n", x, y);
+        printf("2. y(%d) = %d for y = -x + 9, x ∈ (20, + ∞)\n", x, y);
     }
     else
     {
